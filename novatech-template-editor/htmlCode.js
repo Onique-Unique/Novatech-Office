6f41b6b5f52b1498f46c79d4fba2a6b98d9f6359
Onique-Unique <kiddohbeats@yahoo.com>
2022-11-12 13:44:56 -0500

    Content Update

--- novatech-template-editor/htmlCode.js
+++ novatech-template-editor/htmlCode.js
@@ -31,8 +31,8 @@ let htmlCode_first = `<!DOCTYPE html>
                     "@type": "SpeakableSpecification",
                     "xpath": ["/html/head/title", "/html/head/meta[@name='description']/@content"]
                 },
-                "datePublished": "2022-10-15",
-                "dateModified": "2022-10-15",
+                "datePublished": "2022-11-15",
+                "dateModified": "2022-11-15",
                 "author": [{
                     "@type": "Person",
                     "name": "Onique Campbell",
@@ -188,7 +188,7 @@ let htmlCode_first = `<!DOCTYPE html>
                     <div class="h1-blog">                  
                         <h1 class="title"><span>Here it is:</span><b id="share-title"> { .title } </b><span class="yearInsert"></span>! </h1>
                         <div class="date">
-                            <i class="far fa-clock"></i><a href="#">&nbsp; Onique Campbell</a> &bull; <span><time datetime="2022-10-15">Oct 15, 2022</time></span>
+                            <i class="far fa-clock"></i><a href="#">&nbsp; Onique Campbell</a> &bull; <span><time datetime="2022-11-15">Nov 15, 2022</time></span>
                         </div>
                         <div class="breadcrumbs-container" itemscope itemtype="https://schema.org/BreadcrumbList">
                             <ul class="breadcrumbs breadcrumbs-alt" aria-label="Breadcrumbs">

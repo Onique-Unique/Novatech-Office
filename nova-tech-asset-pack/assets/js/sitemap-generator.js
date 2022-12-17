747e7e14672a1b8063f368ac1095622b1b7f96a0
Onique-Unique <kiddohbeats@yahoo.com>
2022-12-17 11:09:53 -0500

    Content Update

--- nova-tech-asset-pack/assets/js/sitemap-generator.js
+++ nova-tech-asset-pack/assets/js/sitemap-generator.js
@@ -1,8 +1,9 @@
 var domain = window.location.hostname;
 var links = document.querySelectorAll('a');
+var OgDomain = "novatechoffice.com";
 var domainXML = `
 <url>
-<loc>https://${domain}/</loc>
+<loc>https://${OgDomain}/</loc>
 <lastmod>${new Date().toISOString()}</lastmod>
 <priority>1.0</priority>
 </url>`;
@@ -10,13 +11,19 @@ var sitemapGen = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://
 var linkHrefs = [];
 
 for (let link of links) {
-  if (link.hostname == domain && !link.href.includes('#')) {
+if (link.hostname == domain && !link.href.includes('#')) {
     var linkHref = link.href.split('.html')[0];
+    // Change hostname to OgDomain
+    linkHref = linkHref.replace(domain, OgDomain);
+    // Remove next 5 characters after novatechoffice.com/ OgDomain
+    linkHref = linkHref.replace(/novatechoffice.com(.{5})/, OgDomain);
+    // Replace any instance of http with https
+    linkHref = linkHref.replace(/http:/g, 'https:');
     if (!linkHrefs.includes(linkHref)) {
-      sitemapGen += '<url>\n<loc>' + linkHref + '</loc>\n<lastmod>' + new Date().toISOString() + '</lastmod>\n<priority>1.0</priority>\n</url>\n';
-      linkHrefs.push(linkHref);
+    sitemapGen += '<url>\n<loc>' + linkHref + '</loc>\n<lastmod>' + new Date().toISOString() + '</lastmod>\n<priority>1.0</priority>\n</url>\n';
+    linkHrefs.push(linkHref);
     }
-  }
+}
 }
 
 sitemapGen += '</urlset>';
@@ -24,8 +31,9 @@ sitemapGen += '</urlset>';
 // Store sitemap data in local storage
 localStorage.setItem('sitemapGen', sitemapGen);
 
-// Create a link for the sitemap
-var linkElement = document.createElement('a');
-linkElement.href = `https://${domain}/view-sitemap.html`;
-linkElement.innerText = 'View Sitemap';
-document.body.appendChild(linkElement);
+// Create a link for the sitemap - Uncomment If Needed
+// var linkElement = document.createElement('a');
+// linkElement.style.color = "aliceblue";
+// linkElement.href = `https://${domain}/view-sitemap.html`;
+// linkElement.innerText = 'View Sitemap';
+// document.body.appendChild(linkElement);
